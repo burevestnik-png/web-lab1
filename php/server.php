2eb49f019a725618e75d5a85b1da6638529dcdf6
Yaroslav <yaroslavsoldat2@gmail.com>
2020-06-27 01:19:41 +0300

    Added mvp server-side script

--- php/server.php
+++ php/server.php
@@ -1,7 +1,30 @@
 <?php
+session_start();
+
 $yValue = $_POST["yValue"];
 $xValue = $_POST["xValue"];
 $rValue = $_POST["rValue"];
 
-$received_data = $yValue . ", " . $xValue . ", " . $rValue;
-echo $received_data;
+$received_data = array($xValue, $yValue, $rValue);
+
+if (!isset($_SESSION['dataHistory'])) {
+    $_SESSION['dataHistory'] = array();
+}
+
+array_push($_SESSION['dataHistory'], $received_data);
+
+echo "<table id=\"dataTable\"><tr>
+                <th>X</th>
+                <th>Y</th>
+                <th>R</th>
+            </tr>";
+
+foreach ($_SESSION['dataHistory'] as $value) {
+    echo "<tr>
+                <td>$value[0]</td>
+                <td>$value[1]</td>
+                <td>$value[2]</td>
+         </tr>";
+}
+
+echo "</table>";
