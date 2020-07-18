e909e74d4353c5ee9386e95fa1870425baf2418a
Yaroslav <yaroslavsoldat2@gmail.com>
2020-07-18 16:53:03 +0300

    we

--- js/main_script.js
+++ js/main_script.js
@@ -1,50 +1,25 @@
-let activeXButton;
 let errorLog;
 let dotTarget;
 let currentRValue;
 let currentYValue;
+let currentXValue;
 let relativeUnit = 0;
 
-let tableProperties = [
-    "x-value-head",
-    "y-value-head",
-    "r-value-head",
-    "current-time-head",
-    "execution-time-head",
-    "hit-result-head"
-]
-
-let NO_X_VALUE_SELECTED_TEXT = "You haven't selected x";
-let FIELDS_Y_AND_R_MUST_BE_NUMBER = "The fields Y and R must be numbers";
-let Y_VALUE_VALIDATE_ERROR = "Y value should be from -3 to 3";
-let R_VALUE_VALIDATE_ERROR = "R value should be from 1 to 4";
+const FIELD_Y_MUST_BE_NUMBER = "The field Y must be number";
+const Y_VALUE_VALIDATE_ERROR = "Y value should be from -3 to 3";
+const X_MUST_BE_CHOSEN = "Value X must be chosen";
+const R_MUST_BE_CHOSEN = "Value R must be chosen";
 
 function validateYValue(yValue) {
     console.log(`Validating: y = ${yValue}`);
 
     if (isNaN(Number(yValue))) {
-        errorLog.text(FIELDS_Y_AND_R_MUST_BE_NUMBER);
+        errorLog.text(FIELD_Y_MUST_BE_NUMBER);
         return false;
     }
 
     if (Number(yValue) < -3 || Number(yValue) > 3) {
         errorLog.text(Y_VALUE_VALIDATE_ERROR);
-        return false;
-    }
-
-    return true;
-}
-
-function validateRValue(rValue) {
-    console.log(`Validating: r = ${rValue}`);
-
-    if (isNaN(Number(rValue))) {
-        errorLog.text(FIELDS_Y_AND_R_MUST_BE_NUMBER);
-        return false;
-    }
-
-    if (Number(rValue) < 1 || Number(rValue) > 4) {
-        errorLog.text(R_VALUE_VALIDATE_ERROR);
         return false;
     }
 
@@ -59,16 +34,6 @@ function getY() {
     }
 
     return checkDouble(yValue);
-}
-
-function getR() {
-    let rValue = $('#r-value').val();
-
-    if (rValue === "") {
-        rValue = "emptyString";
-    }
-
-    return checkDouble(rValue);
 }
 
 function checkDouble( value ) {
@@ -91,18 +56,10 @@ $(document).ready(function () {
     errorLog = $('#error-text');
     dotTarget = $('#target-dot');
 
-    $(".x-button").on('click',function () {
-        if (activeXButton === undefined ? undefined : activeXButton.attr("id") === $(this).attr("id")) {
-            $(this).removeClass("active");
-            activeXButton = undefined;
-            return;
-        }
-        const xButtonList = $(".x-button");
+    $("input[type=radio][name=\"x-group\"]").on('click',function () {
+        errorLog.text("");
 
-        xButtonList.toArray().forEach(xButton => xButton.classList.remove("active"));
-        $(this).addClass("active");
-
-        activeXButton = $(this);
+        currentXValue = $(this).val();
 
         if (currentRValue === undefined || currentYValue === undefined) {
             return;
@@ -112,19 +69,15 @@ $(document).ready(function () {
 
         dotTarget.attr("r", 3);
         dotTarget.attr("cy", calculateY(currentYValue));
-        dotTarget.attr("cx", calculateX(activeXButton.text()));
+        dotTarget.attr("cx", calculateX($(this).val()));
     });
 
-    $('#r-value').change(function () {
+    $('input[type=radio][name="r-group"]').change(function () {
         errorLog.text("");
 
-        if (!validateRValue(getR())) {
-            return;
-        }
+        currentRValue = $(this).val();
 
-        currentRValue = getR();
-
-        if (activeXButton === undefined || currentYValue === undefined) {
+        if (currentXValue === undefined || currentYValue === undefined) {
             return;
         }
 
@@ -132,19 +85,20 @@ $(document).ready(function () {
 
         dotTarget.attr("r", 3);
         dotTarget.attr("cy", calculateY(currentYValue));
-        dotTarget.attr("cx", calculateX(activeXButton.text()));
+        dotTarget.attr("cx", calculateX(currentXValue));
     });
 
     $('#y-value').change(function () {
         errorLog.text("");
 
         if (!validateYValue(getY())) {
+            dotTarget.attr("r", 0);
             return;
         }
 
         currentYValue = getY();
 
-        if (activeXButton === undefined || currentRValue === 0) {
+        if (currentRValue === undefined) {
             return;
         }
 
@@ -152,24 +106,29 @@ $(document).ready(function () {
 
         dotTarget.attr("r", 3);
         dotTarget.attr("cy", calculateY(currentYValue));
-        dotTarget.attr("cx", calculateX(activeXButton.text()));
+        dotTarget.attr("cx", calculateX(currentXValue));
     });
 
     $("#submit-button").on('click', function () {
         errorLog.text("");
 
-        if (activeXButton === undefined) {
-            errorLog.text(NO_X_VALUE_SELECTED_TEXT);
+        let xValue = $('input[name="x-group"]:checked').val();
+        let yValue = checkDouble(getY());
+        let rValue = $('input[name="r-group"]:checked').val();
+
+        if (xValue === undefined) {
+            errorLog.text(X_MUST_BE_CHOSEN);
             return;
         }
 
-        let xValue = activeXButton.text();
-        let yValue = checkDouble(getY());
-        let rValue = checkDouble(getR());
+        if (rValue === undefined) {
+            errorLog.text(R_MUST_BE_CHOSEN);
+            return;
+        }
 
         console.log(`Got data: x = ${xValue}, y = ${yValue}, r = ${rValue}`);
 
-        if (!validateRValue(rValue) || !validateYValue(yValue)) {
+        if (!validateYValue(yValue)) {
             return;
         }
 
@@ -190,30 +149,15 @@ $(document).ready(function () {
     });
 
     $('#reset-button').on('click', () => {
-        if (activeXButton !== undefined) {
-            activeXButton.removeClass("active");
-        }
-        $('#r-value').val("");
         $('#y-value').val("");
-
-        $('.r-value-label').removeClass('active-input');
         $('.y-value-label').removeClass('active-input');
+        $('input[name="x-group"]:checked').prop('checked', false);
+        $('input[name="r-group"]:checked').prop('checked', false);
 
         dotTarget.attr("r", 0);
         currentYValue = undefined;
         currentRValue = undefined;
-        activeXButton = undefined;
-    });
-
-    $('.r-value-group').on('focusin', function () {
-        $(this).find('.r-value-label').addClass('active-input');
-    });
-
-
-    $('.r-value-group').on('focusout', function () {
-        if (!$('#r-value').val()) {
-            $(this).find('.r-value-label').removeClass('active-input');
-        }
+        currentXValue = undefined;
     });
 
     $('.y-value-group').on('focusin', function () {
